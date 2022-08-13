c2e7210a85f8c693efe5fc05da53b0545873415c
mali-28 <alihadees786@gmail.com>
2022-08-13 17:19:29 +0500

    fouth commit

--- sextant/src/Content.jsx
+++ sextant/src/Content.jsx
@@ -2,6 +2,7 @@ import { data } from "./utils/data";
 import Card from "./components/Card";
 import Footer from "./components/Footer";
 import IP from "./components/IP";
+import WebSocket from "./components/WebSocket";
 const Content = () => {
     return (
         <div className="content my-5">
@@ -9,6 +10,8 @@ const Content = () => {
                 <h2 className="primary_color center text-center mb-3">Welcome to our Site</h2>
                 <IP ipVal="ipv4"/>
                 <IP ipVal="ipv6"/>
+                <WebSocket/>
+
                 <div className="flex flex-wrap mb-3">
                     {
                         data?.map((val)=>{
